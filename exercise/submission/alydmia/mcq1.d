0aabddac5703b9155f778ae72f1c0c3b9bca0b33
Alya Damia Huzaimy <146603392+alydmia@users.noreply.github.com>
2024-01-08 10:27:02 +0800

    Update mcq1.d

--- exercise/submission/alydmia/mcq1.d
+++ exercise/submission/alydmia/mcq1.d
@@ -1,1 +1,27 @@
+## MCQ Set: 1
 
+**Name: ALYA DAMIA BINTI HUZAIMY**
+
+**Matrix No: A21EC0159**
+
+## Answer
+1. B
+2. A
+3. A
+4. A
+5. A
+6. A
+7. A
+8. D
+9. A
+10. B
+11. A
+12. B
+13. C
+14. D
+15. D
+16. C
+17. D
+18. D
+19. C
+20. B
